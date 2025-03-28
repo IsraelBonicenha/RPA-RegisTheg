eff1271323a7ef76f79e532ce74982cad1f4f6fb
Israel Bonicenha <israelbonicenha2020@gmail.com>
2025-03-28 17:01:38 -0300

    Adicionando DtgManager e método para verificar alterações

---
+++ FrmInsercao/Utils/DtgManager.cs
@@ -0,0 +1,17 @@
+﻿namespace FrmInsercao.Utils
+{
+    public class DtgManager
+    {
+        public void VeriricarAlteracaoDtg(int _linesDtgInicial, int linesAtual)
+        {
+            if (linesAtual != _linesDtgInicial)
+            {
+                var respostaUser = MessageBox.Show("Deseja salvar as alterações feitas na tabela?",
+                    "CONFIRMAÇÃO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (respostaUser == DialogResult.Yes) { }
+                //SalvarAlteracoesTable();
+            }
+        }
+    }
+}
